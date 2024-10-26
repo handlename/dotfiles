f729c5799ade4d1d9b671ebd784adfd6bc3aaceb
NAGATA Hiroaki <nagata@handlena.me>
2024-10-27 00:02:58 +0900

    feat: install darwin apps, more

--- darwin.nix
+++ darwin.nix
@@ -12,6 +12,46 @@
   system = {
     defaults = {
       NSGlobalDomain.AppleShowAllExtensions = true;
+
+      finder = {
+        AppleShowAllFiles = true;
+        AppleShowAllExtensions = true;
+      };
+
+      dock = {
+        autohide = true;
+        show-recents = false;
+        orientation = "left";
+      };
+    };
+  };
+
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = true;
+      # enable after almost all appliactions are installed by nix
+      # cleanup = "zap";
+    };
+
+    casks = [
+      "1password-cli"
+      "alfred"
+      "appcleaner"
+      "arc"
+      "bettertouchtool"
+      "dash"
+      "docker"
+      "font-monaspace-nerd-font"
+      "google-japanese-ime"
+      "obsidian"
+      "toggl-track"
+    ];
+
+    masApps = {
+      kindle = 302584613;
+      reeder = 6475002485;
     };
   };
 }
