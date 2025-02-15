48c37cbb99ac2dcd3af7827f6d501316a1a0d44d
NAGATA Hiroaki <nagata@handlena.me>
2025-02-15 22:25:32 +0900

    [zellij] simplify config file placement

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -1,24 +1,13 @@
 {
   config,
-  lib,
-  pkgs,
   ...
 }:
 
-with lib;
-
-let
-  vars = import ../../vars.nix;
-  moduleHome = toString ./.;
-in
 {
   programs.zellij.enable = true;
 
-  home.activation = {
-    # symlink config because programs.zellij not supports kdl format
-    zellijSymlinkConfig = lib.mkAfter ''
-      run mkdir -p ${config.xdg.configHome}/zellij
-      run ln -sf ${moduleHome}/config.kdl ${config.xdg.configHome}/zellij/config.kdl
-    '';
+  # place file because programs.zellij not supports kdl format
+  home.file = {
+    "${config.xdg.configHome}/zellij/config.kdl".source = ./config.kdl;
   };
 }
