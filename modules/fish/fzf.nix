f606ff36bdda226915a4b29a35061e9d9bb3db0c
NAGATA Hiroaki <nagata@handlena.me>
2025-06-29 10:18:20 +0900

    [fish] remove unsupported functions

--- modules/fish/fzf.nix
+++ modules/fish/fzf.nix
@@ -14,8 +14,5 @@ in
   programs.fish.shellInit = ''
     set -U FZF_LEGACY_KEYBINDINGS 0
     set -x FZF_DEFAULT_OPTS '--layout=reverse --height 40%'
-
-    bind --erase \cf __fzf_find_file
-    bind --erase --mode insert \cf __fzf_find_file
   '';
 }
