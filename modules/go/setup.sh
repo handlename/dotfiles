813fcf8b846903e25f79e4081d81a26ceba8e960
NAGATA Hiroaki <nagata@handlena.me>
2025-11-30 20:06:29 +0900

    [go] install delve

--- modules/go/setup.sh
+++ modules/go/setup.sh
@@ -3,6 +3,7 @@
 set -eux
 
 tools=(
+    'github.com/go-delve/delve/cmd/dlv@latest'
     'golang.org/x/tools/cmd/goimports@latest'
     'golang.org/x/tools/gopls@latest'
     'gotest.tools/gotestsum@latest'
