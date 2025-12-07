1e5fea2349aeb5678f1fb29ecd40f265276d6d59
NAGATA Hiroaki <nagata@handlena.me>
2025-12-07 21:52:10 +0900

    [hammerspoon] app switch

---
+++ modules/hammberspoon/modules/app_switch.lua
@@ -0,0 +1,19 @@
+local apps = {
+    { key = "G", id = "md.obsidian" },
+    { key = "K", id = "com.toggl.daneel" },
+    { key = "L", id = "com.kapeli.dashdoc" },
+
+    { key = "H", id = "dev.zed.Zed" },
+    { key = "T", id = "org.alacritty" },
+    { key = "N", id = "app.zen-browser.zen" },
+
+    { key = "M", id = "com.anthropic.claudefordesktop" },
+    { key = "W", id = "com.1password.1password" },
+    { key = "V", id = "com.tinyspeck.slackmacgap" },
+}
+
+for _, item in ipairs(apps) do
+    hs.hotkey.bind({ "ctrl", "shift", "cmd" }, item.key, function()
+        hs.application.launchOrFocusByBundleID(item.id)
+    end)
+end
