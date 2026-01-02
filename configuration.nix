9eb8851df8e367a22188f150a684a98cfcea5ee0
NAGATA Hiroaki <nagata@handlena.me>
2026-01-02 22:27:37 +0900

    feat: disable generate man cache
    to avoid waiting cache generation

--- configuration.nix
+++ configuration.nix
@@ -2,4 +2,7 @@
   system.stateVersion = 5;
   nixpkgs.config.allowUnfree = true;
   nixpkgs.hostPlatform = "aarch64-darwin";
+
+  # disable cache generation to speed up switch.
+  documentation.man.generateCaches = false;
 }
