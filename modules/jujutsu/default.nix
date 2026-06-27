abfdc5b8bb75331852ae366d0480e2dc2788386e
NAGATA Hiroaki <nagata@handlena.me>
2026-06-27 21:42:55 +0900

    [jj] gpg sign

--- modules/jujutsu/default.nix
+++ modules/jujutsu/default.nix
@@ -10,6 +10,15 @@
         name = "NAGATA Hiroaki";
         email = "nagata@handlena.me";
       };
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        backends.ssh.program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINTVcp7Sd0Z99l0sQ6wIvaS4sq7an3AnpZ3ZOxZfxwWT";
+      };
+      git = {
+        "sign-on-push" = true;
+      };
     };
   };
 
