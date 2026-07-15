10cc059e522ef7e587ac8715846a75f9250e5748
NAGATA Hiroaki <nagata@handlena.me>
2026-07-16 08:12:57 +0900

    [zed] avoid full build

--- modules/zed/default.nix
+++ modules/zed/default.nix
@@ -9,6 +9,10 @@
     enable = true;
     package = null;
 
+    # To avoid full build
+    # Zed will be installed via Homebrew cask
+    package = null;
+
     extensions = import ./extensions.nix;
     userSettings = import ./settings.nix { inherit username; };
     userKeymaps = import ./keymap.nix;
